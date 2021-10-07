a0f228eeebecad2d79e8f4fe9e5548c934317c03
Bicalheira <bicalheira@users.noreply.github.com>
2021-10-07 20:52:32 -0300

    added cpf validator

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -44,11 +44,18 @@ const errorMessages = {
 		valueMissing: "O campo de data de nascimento não pode estar vazio",
 		customError: "Você deve ser maior que dezoito (18) anos para se cadastrar",
 	},
+	cpf: {
+		valieMissing: "O campo de CPF não pode estar vazio",
+		customError: "O CPF digitado não é válido",
+	},
 };
 
 const validators = {
 	birthday: (input) => {
 		validateBirthday(input);
+	},
+	cpf: (input) => {
+		validateCpf(input);
 	},
 };
 
@@ -79,4 +86,80 @@ function ageMajority(date) {
 	const ageMajority = new Date(date.getUTCFullYear() + 18, date.getUTCMonth() + date.getUTCDate());
 
 	return ageMajority <= actualDate;
+}
+
+function validateCpf(input) {
+	const formattedCpf = input.value.replace(/\D/g, "");
+
+	let message = "";
+
+	if (!checkRepeatedCpf(formattedCpf) || !checkCpfStructure(formattedCpf)) {
+		message = "O CPF digitado não é válido";
+	}
+
+	input.setCustomValidity(message);
+}
+
+function checkRepeatedCpf(cpf) {
+	const repeatedValues = [
+		"00000000000",
+		"11111111111",
+		"22222222222",
+		"33333333333",
+		"44444444444",
+		"55555555555",
+		"66666666666",
+		"77777777777",
+		"88888888888",
+		"99999999999",
+	];
+
+	let validateCpf = true;
+
+	repeatedValues.forEach((value) => {
+		if (value === cpf) {
+			validateCpf = false;
+		}
+	});
+
+	return validateCpf;
+}
+
+function checkCpfStructure(cpf) {
+	const multiply = 10;
+
+	return checkVerifierNumber(cpf, multiply);
+}
+
+function checkVerifierNumber(cpf, multiply) {
+	if (multiply >= 12) {
+		return true;
+	}
+
+	let sum = 0;
+	let aux = multiply;
+
+	const cpfNumber = cpf.substr(0, multiply - 1).split("");
+	const verifierDigit = cpf.charAt(multiply - 1);
+
+	for (let i = 0; aux > 1; aux--) {
+		sum += cpfNumber[i] * aux;
+		i++;
+	}
+
+	if (verifierDigit == digitConfirm(sum)) {
+		return checkVerifierNumber(cpf, multiply + 1);
+	}
+
+	return false;
+}
+
+function digitConfirm(sum) {
+	let remainder = 11 - (sum % 11);
+
+	if (remainder === 10 || remainder === 11) {
+		remainder = 0;
+	}
+
+	return remainder;
 }
