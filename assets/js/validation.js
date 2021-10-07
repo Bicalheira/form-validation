352968853863da13d8ddcf2ed913dc1f3bddf248
Bicalheira <bicalheira@users.noreply.github.com>
2021-10-06 22:34:40 -0300

    adding error message handling in form

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -4,13 +4,65 @@ export function validate(input) {
 	if (validators[inputType]) {
 		validators[inputType](input);
 	}
+
+	if (input.validity.valid) {
+		input.parentElement.classList.remove("input-container--invalido");
+		input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+	} else {
+		input.parentElement.classList.add("input-container--invalido");
+		input.parentElement.querySelector(".input-mensagem-erro").innerHTML = showErrorMessage(inputType, input);
+	}
 }
+
+const errorTypes = [
+	// "badInput",
+	"customError",
+	"patternMismatch",
+	// "rangeOverflow",
+	// "rangeUnderflow",
+	// "stepMismatch",
+	// "tooLong",
+	// "tooShort",
+	"typeMismatch",
+	"valueMissing",
+];
+
+const errorMessages = {
+	name: {
+		valueMissing: "O campo nome não pode estar vazio",
+	},
+	email: {
+		valueMissing: "O campo e-mail não pode estar vazio",
+		typeMismatch: "O e-mail digitado não é valido",
+	},
+	password: {
+		valueMissing: "O campo senha não pode estar vazio",
+		patternMismatch:
+			"A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiuscula, um número e não deve conter simbolos",
+	},
+	birthday: {
+		valueMissing: "O campo de data de nascimento não pode estar vazio",
+		customError: "Você deve ser maior que dezoito (18) anos para se cadastrar",
+	},
+};
 
 const validators = {
 	birthday: (input) => {
 		validateBirthday(input);
 	},
 };
+
+function showErrorMessage(inputType, input) {
+	let message = "";
+
+	errorTypes.forEach((error) => {
+		if (input.validity[error]) {
+			message = errorMessages[inputType][error];
+		}
+	});
+
+	return message;
+}
 
 function validateBirthday(input) {
 	const getDate = new Date(input.value);
